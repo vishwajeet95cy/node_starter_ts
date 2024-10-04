7950ad0306bdcfe9226bd3ea588393636d93b264
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-10-04 17:08:13 +0530

    feat: express server index added

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import express from "express"; // Import the Express framework
+import express from "express";
 
-require("dotenv").config(); // Load environment variables from a .env file into process.env
-const cors = require("cors"); // Import the CORS middleware
-const app = express(); // Create an Express application instance
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
 
-app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS) for all routes
+app.use(cors());
 
-const PORT = process.env.PORT || 8000; // Set the server's port from environment variables or default to 8000
+const PORT = process.env.PORT || 8000;
 
-app.use(express.json()); // Parse incoming JSON requests and make the data available in req.body
+app.use(express.json());
 
 app.listen(PORT, () => {
-  // Start the server and listen on the specified port
-  console.log(`Server is running on http://localhost:${PORT}`); // Log a message indicating the server is running
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
