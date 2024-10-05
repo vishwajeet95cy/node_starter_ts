ae1dd92cffc1428665aeac04f4895646c37f9948
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-10-05 14:45:43 +0530

    feat: First Api With Custom Data Added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 
 require("dotenv").config();
 const cors = require("cors");
-const app = express();
+const app: Application = express();
 
 app.use(cors());
 
@@ -12,4 +12,18 @@ app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// GET REQUEST
+app.get("/customers", (req: Request, res: Response) => {
+  const customers = [
+    { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
+    {
+      name: "Joel Smith",
+      email: "joel.smith@example.com",
+      phone: "+0987654321",
+    },
+  ];
+
+  return res.status(200).json(customers);
 });
